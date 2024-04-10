d51d1e25c18da4968c1c3532d0721d7f849e4529
AnmoL-09 <anmolmahobiya092@gmail.com>
2024-04-10 20:08:54 +0530

    header completed

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './Hero.css';
 import profile_img from '../../assets/profile_img.svg';
+import myPfp from '../../assets/pfpMe.png';
 
 
 const Hero = () => {
   return (
     <div className='hero'>
-        <img src={profile_img} alt="" />
+        <img src={myPfp} alt="" />
+        <h1><span>I'm Anmol,</span> MERN Stack developer and UI / UX Designer</h1>
+        <p>     
+          I'm a MERN Stack developer with expertise in UI/UX design, dedicated to crafting seamless web solutions. Passionate about innovation and open-source contributions, I'm here to bring your digital projects to life.
+        </p>
+        <div className="hero-action">
+          <div className="hero-connect">Connect with me</div>
+          <div className="hero-resume">My Resume</div>
+        </div>
     </div>
   )
 }
