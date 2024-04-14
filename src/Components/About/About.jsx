60ea723996137e8a02866dbeff35d2b1464cea1a
AnmoL-09 <anmolmahobiya092@gmail.com>
2024-04-14 10:04:53 +0530

    added funtionality to navbar links

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,7 +6,7 @@ import bg from '../../assets/bg.png'
 
 const About = () => {
   return (
-    <div className='about'>
+    <div className='about' id='about'>
         <div className="about-title">
             <h1>About me</h1>
             <img src={theme_pattern} alt="" />
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import './Hero.css';
 import profile_img from '../../assets/profile_img.svg';
 import myPfp from '../../assets/pfpMe.png';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className='hero' id='home'>
         <img src={myPfp} alt="" />
         <h1><span>I'm Anmol,</span> MERN Stack developer and UI / UX Designer</h1>
         <p>     
           I'm a MERN Stack developer with expertise in UI/UX design, dedicated to crafting seamless web solutions. Passionate about innovation and open-source contributions, I'm here to bring your digital projects to life.
         </p>
         <div className="hero-action">
-          <div className="hero-connect">Connect with me</div>
+          <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
           <div className="hero-resume">My Resume</div>
         </div>
     </div>
